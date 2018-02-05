c1c59039011f64fc516d5fbaac1c654982daea3c
tiabas <kevin@urbandoor.co>
2018-02-04 23:38:21 -0800

    Remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Nav from 'react-bootstrap/lib/Nav';
+import Navbar from 'react-bootstrap/lib/Navbar';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Grid className="App" style={{ backgroundColor:"red" }}>
+          <Row className="show-grid">
+            <Col xs={12} md={8} lg={8} style={{ backgroundColor:"purple" }}>
+              Rest of the code goes here
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
